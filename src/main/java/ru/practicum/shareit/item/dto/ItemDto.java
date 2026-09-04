448bc8d4b0560415f5c0c6d5421cda7aef19accd
Nytik2002 <NytLocki@yandex.ru>
2026-09-05 00:48:39 +0500

    Добавлены данные бронирований в ItemDto

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.practicum.shareit.booking.dto.BookingShortDto;
 
 @Data
 @Builder
@@ -16,4 +17,6 @@ public class ItemDto {
     private Boolean available;
     private Long ownerId;
     private Long requestId;
+    private BookingShortDto lastBooking;
+    private BookingShortDto nextBooking;
 }
