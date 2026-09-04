cd93de8e9319a5fe3a9955a090d14578ae4f5b54
Nytik2002 <NytLocki@yandex.ru>
2026-09-04 22:03:27 +0500

    Реализован POST запрос для бронирований, BookingController

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,12 +1,30 @@
 package ru.practicum.shareit.booking;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.NewBookingRequest;
+import ru.practicum.shareit.booking.service.BookingService;
+import ru.practicum.shareit.constants.ShareitConstants;
 
-/**
- * TODO Sprint add-bookings.
- */
+//Контроллер для работы с бронированиями
 @RestController
-@RequestMapping(path = "/bookings")
+@RequestMapping("/bookings")
 public class BookingController {
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    //Создание нового бронирования
+    @PostMapping
+    public BookingDto create(
+            @RequestHeader(ShareitConstants.XSharerID) Long userId,
+            @RequestBody NewBookingRequest bookingRequest) {
+        return bookingService.create(userId, bookingRequest);
+    }
 }
