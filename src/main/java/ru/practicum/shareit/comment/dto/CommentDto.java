9936d7b8d11c07e7b1cfb5bb915a4238592925d6
Nytik2002 <NytLocki@yandex.ru>
2026-09-07 01:59:14 +0500

    Добавила ограничение длины комментария

--- src/main/java/ru/practicum/shareit/comment/dto/CommentDto.java
+++ src/main/java/ru/practicum/shareit/comment/dto/CommentDto.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.comment.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +18,7 @@ public class CommentDto {
     private Long id;
 
     @NotBlank
+    @Size(max = 2000)
     private String text;
 
     private String authorName;
